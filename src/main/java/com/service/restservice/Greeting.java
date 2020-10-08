2e3636ba2937ccebbd6cdefa79e0ea4e0f8f3b22
MokoenaLee <Great19*!123>
2020-10-08 07:21:52 -0600

    Initial commit - service entry point and main logic working successfully

---
+++ src/main/java/com/service/restservice/Greeting.java
@@ -0,0 +1,21 @@
+package com.service.restservice;
+
+public class Greeting {
+
+    private final long id;
+    private final String content;
+
+
+    public Greeting(long id, String content) {
+        this.content = content;
+        this.id = id;
+    }
+
+    public long getId() {
+        return id;
+    }
+
+    public String getContent() {
+        return content;
+    }
+}
